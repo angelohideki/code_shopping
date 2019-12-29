4d066b8d820e8e8c88792870454be5fd141bf456
Angelo Hideki Noda <angelonoda@gmail.com>
2019-12-29 19:26:56 -0300

    'criando_categorias_para_teste'

--- database/migrations/2019_12_29_215930_create_categories_table.php
+++ database/migrations/2019_12_29_215930_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModelsCategoriesTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateModelsCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('models_categories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateModelsCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('models_categories');
+        Schema::dropIfExists('categories');
     }
 }
