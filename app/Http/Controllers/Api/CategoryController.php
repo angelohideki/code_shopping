aad34668f72f8b8f081c90ce34ae599e1d13f202
Angelo Hideki Noda <angelonoda@gmail.com>
2019-12-29 18:42:19 -0300

    'configuracao_de_controllers_resource'

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CodeShopping\Http\Controllers\Api;
+
+use CodeShopping\Models\Category;
+use CodeShopping\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \CodeShopping\Models\Category  $modelsCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $modelsCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \CodeShopping\Models\Category  $modelsCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $modelsCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \CodeShopping\Models\Category  $modelsCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $modelsCategory)
+    {
+        //
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace CodeShopping;
+namespace CodeShopping\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ModelsCategory extends Model
+class Category extends Model
 {
     //
 }
