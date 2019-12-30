ceb21b9e009fcc9a2a51394cf8d10f79b94e3887
Angelo Hideki Noda <angelonoda@gmail.com>
2019-12-30 00:00:55 -0300

    'recuperando_categorias'

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,57 +8,27 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Category::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \CodeShopping\Models\Category  $modelsCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $modelsCategory)
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    public function update(Request $request, Category $category)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \CodeShopping\Models\Category  $modelsCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $modelsCategory)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \CodeShopping\Models\Category  $modelsCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $modelsCategory)
+    public function destroy(Category $category)
     {
         //
     }
