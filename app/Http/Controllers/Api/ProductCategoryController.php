8576e28ca1f1f0135e638cf992373a3293cde2c0
Angelo Hideki Noda <angelonoda@gmail.com>
2020-01-01 19:40:26 -0300

    'endpoint_para_inserir_categorias_em_produtos'

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -3,7 +3,9 @@
 namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Models\Category;
 use CodeShopping\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class ProductCategoryController extends Controller
@@ -13,9 +15,13 @@ class ProductCategoryController extends Controller
         return $product->categories;
     }
 
-    public function store(Request $request)
+    public function store(Request $request, Product $product)
     {
-        //
+        $changed = $product->categories()->sync($request->categories);
+        $categoriesAttachedId = $changed['attached'];
+        /** @var Collection $categories */
+        $categories = Category::whereIn('id',$categoriesAttachedId)->get();
+        return $categories->count() ? response()->json($categories, 201) : [];
     }
 
     public function destroy($id)
