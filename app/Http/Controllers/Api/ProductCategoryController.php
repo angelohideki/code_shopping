3f8d9e7d2977bb190198aca247315c76591f63fb
Angelo Hideki Noda <angelonoda@gmail.com>
2020-01-01 23:21:02 -0300

    'criando_serializador_para_relacionamento'

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -4,6 +4,7 @@ namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
 use CodeShopping\Http\Requests\ProductCategoryRequest;
+use CodeShopping\Http\Resources\ProductCategoryResource;
 use CodeShopping\Models\Category;
 use CodeShopping\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
@@ -13,7 +14,7 @@ class ProductCategoryController extends Controller
 {
     public function index(Product $product)
     {
-        return $product->categories;
+        return  new ProductCategoryResource($product);
     }
 
     public function store(ProductCategoryRequest $request, Product $product)
@@ -22,7 +23,7 @@ class ProductCategoryController extends Controller
         $categoriesAttachedId = $changed['attached'];
         /** @var Collection $categories */
         $categories = Category::whereIn('id',$categoriesAttachedId)->get();
-        return $categories->count() ? response()->json($categories, 201) : [];
+        return $categories->count() ? response()->json(new ProductCategoryResource($product), 201) : [];
     }
 
     public function destroy(Product $product, Category $category)
