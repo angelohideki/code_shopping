28895fde64fa5455fc020ca6aee72112a25485a8
Angelo Hideki Noda <angelonoda@gmail.com>
2020-01-02 22:56:15 -0300

    'orientacao_por_eventos'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace CodeShopping\Providers;
 
+use CodeShopping\Models\ProductInput;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+        ProductInput::created(function ($input){
+            $product = $input->product;
+            $product->stock += $input->amount;
+            $product->save();
+        });
     }
 
     /**
