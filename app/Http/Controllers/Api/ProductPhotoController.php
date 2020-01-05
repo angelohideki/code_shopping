859882f3710d81048bdc24e66157f849f98d1e6b
Angelo Hideki Noda <angelonoda@gmail.com>
2020-01-05 00:53:00 -0300

    'resource_collection'

--- app/Http/Controllers/Api/ProductPhotoController.php
+++ app/Http/Controllers/Api/ProductPhotoController.php
@@ -3,6 +3,7 @@
 namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Http\Resources\ProductPhotoCollection;
 use CodeShopping\Http\Resources\ProductPhotoResource;
 use CodeShopping\Models\Product;
 use CodeShopping\Models\ProductPhoto;
@@ -13,7 +14,7 @@ class ProductPhotoController extends Controller
 
     public function index(Product $product)
     {
-        return ProductPhotoResource::collection($product->photos);
+        return new ProductPhotoCollection($product->photos,$product);
     }
 
     public function store(Request $request)
@@ -23,10 +24,10 @@ class ProductPhotoController extends Controller
 
     public function show(Product $product, ProductPhoto $photo)
     {
-        if($photo->productId != $product->id){
+        if($photo->product_id != $product->id){
             abort(404);
         }
-        return $photo;
+        return new ProductPhotoResource($photo);
     }
 
     public function update(Request $request, ProductPhoto $productPhoto)
