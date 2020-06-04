6c7d77c64294ca51f97bf837894bda2170dd64b5
Angelo Hideki Noda <angelonoda@gmail.com>
2020-06-03 22:09:47 -0300

    Listando categorias no componente

--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -8,7 +8,7 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CategoryListComponent implements OnInit {
 
-  categories = [];
+  categories: Array<{id: number, name: string, active:boolean, created_at:{date:string}}> = [];
 
   constructor(private  http: HttpClient) {
 
@@ -16,13 +16,13 @@ export class CategoryListComponent implements OnInit {
 
   ngOnInit(): void {
     const  token = window.localStorage.getItem('token');
-    this.http.get<Array<any>>('http://localhost:8000/api/categories', {
+    this.http.get<{data:Array<{id: number, name: string, active:boolean, created_at:{date:string}}>}>('http://localhost:8000/api/categories', {
        headers:{
          //'Authorization' : 'Bearer ' + token
          'Authorization' : `Bearer ${token}`
        }
     })
-      .subscribe(data => this.categories = data);
+      .subscribe(response => this.categories = response.data);
   }
 
 }
